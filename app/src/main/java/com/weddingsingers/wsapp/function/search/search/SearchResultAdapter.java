929721b3b3f60f3666da897f2f8c5b97f7a44b8f
KSJ <smart23033@gmail.com>
2016-08-25 18:04:35 +0900

    SearchResultAdapter 생성

--- app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchResultAdapter.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchResultAdapter.java
@@ -3,5 +3,6 @@ package com.weddingsingers.wsapp.function.search.search;
 /**
  * Created by Tacademy on 2016-08-25.
  */
-public class SearchResultAdapter {
+public class SearchResultAdapter{
+//리싸이클러 만들자!!
 }
