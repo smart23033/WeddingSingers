1a805ae5da1b6d123f70b5caf4d337bfa6a8aa02
KSJ <smart23033@gmail.com>
2016-08-25 17:53:54 +0900

    Data class 생성

---
+++ app/src/main/java/com/weddingsingers/wsapp/data/Payment.java
@@ -0,0 +1,7 @@
+package com.weddingsingers.wsapp.data;
+
+/**
+ * Created by Tacademy on 2016-08-25.
+ */
+public class Payment {
+}
---
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchResultAdapter.java
@@ -0,0 +1,7 @@
+package com.weddingsingers.wsapp.function.search.search;
+
+/**
+ * Created by Tacademy on 2016-08-25.
+ */
+public class SearchResultAdapter {
+}
