c0ab4b6083d2600f39a59246b17d0a5c6d10ee22
KSJ <smart23033@gmail.com>
2016-08-25 17:30:10 +0900

    SearchActivity에 검색버튼 누르기까지 완성!

--- app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchActivity.java
@@ -1,6 +1,7 @@
 package com.weddingsingers.wsapp.function.search.search;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -20,9 +22,16 @@ import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
 
+    final static String RECENT_SEARCH_FRAGMENT = "RecentSearchFragment";
+    final static String FILTER_FRAGMENT = "FilterFragment";
+    final static String SEARCH_RESULT_FRAGMENT = "SearchResultFragment";
+
+
+    @BindView(R.id.search_et_keyword)
+    EditText keywordView;
 
     @BindView(R.id.search_tv_keyword)
-    EditText keywordView;
+    TextView keywordTextView;
 
     @BindView(R.id.search_toggle_filter)
     ToggleButton filterBtn;
@@ -43,8 +52,8 @@ public class SearchActivity extends AppCompatActivity {
 
         FragmentTransaction ft = getSupportFragmentManager()
                 .beginTransaction();
-        RecentSearchFragment f = new RecentSearchFragment();
-        ft.add(R.id.act_search_fl_container,f);
+        RecentSearchFragment recentSearchFragment = new RecentSearchFragment();
+        ft.add(R.id.act_search_fl_container,recentSearchFragment, RECENT_SEARCH_FRAGMENT);
         ft.commit();
 
 //        두번째 클릭 부터 반응??
@@ -55,13 +64,13 @@ public class SearchActivity extends AppCompatActivity {
                     FragmentTransaction ft = getSupportFragmentManager()
                             .beginTransaction();
                     FilterFragment filterFragment = new FilterFragment();
-                    ft.replace(R.id.act_search_fl_container, filterFragment);
+                    ft.replace(R.id.act_search_fl_container, filterFragment, FILTER_FRAGMENT);
                     ft.commit();
                 }else{
                     FragmentTransaction ft = getSupportFragmentManager()
                             .beginTransaction();
                     RecentSearchFragment recentSearchFragment = new RecentSearchFragment();
-                    ft.replace(R.id.act_search_fl_container, recentSearchFragment);
+                    ft.replace(R.id.act_search_fl_container, recentSearchFragment, RECENT_SEARCH_FRAGMENT);
                     ft.commit();
                 }
             }
@@ -80,12 +89,36 @@ public class SearchActivity extends AppCompatActivity {
              case R.id.main_menu_search: {
                  Toast.makeText(SearchActivity.this,"SearchActivity's Search Button Clicked", Toast.LENGTH_SHORT).show();
 
-                 FragmentTransaction ft = getSupportFragmentManager()
-                         .beginTransaction();
-                 SearchResultFragment searchResultFragment = new SearchResultFragment();
-                 ft.replace(R.id.act_search_fl_container, searchResultFragment);
-                 ft.commit();
+                 Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.act_search_fl_container);
 
+                if(currentFragment.getTag() != SEARCH_RESULT_FRAGMENT) {
+//               searchResultFragment가 아닌 프레그먼트에서 search버튼 누를 때
+                    keywordTextView.setText(keywordView.getText());
+                    keywordTextView.setVisibility(View.VISIBLE);
+
+                    keywordView.setText("");
+                    keywordView.setVisibility(View.GONE);
+                    filterBtn.setVisibility(View.GONE);
+
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    SearchResultFragment searchResultFragment = new SearchResultFragment();
+                    ft.replace(R.id.act_search_fl_container, searchResultFragment , SEARCH_RESULT_FRAGMENT);
+                    ft.commit();
+                }else {
+//               searchResultFragment에서 search버튼 누를 때
+                    keywordTextView.setText("");
+                    keywordTextView.setVisibility(View.GONE);
+
+                    keywordView.setVisibility(View.VISIBLE);
+                    filterBtn.setVisibility(View.VISIBLE);
+
+                    FragmentTransaction ft = getSupportFragmentManager()
+                            .beginTransaction();
+                    RecentSearchFragment recentSearchFragment = new RecentSearchFragment();
+                    ft.replace(R.id.act_search_fl_container, recentSearchFragment, RECENT_SEARCH_FRAGMENT);
+                    ft.commit();
+                }
                  return true;
              }
          }
