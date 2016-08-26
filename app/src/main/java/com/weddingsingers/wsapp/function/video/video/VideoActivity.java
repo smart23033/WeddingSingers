24cd2b7f3b2421878e0e3bb27f0f4e02eb46c3c7
KSJ <smart23033@gmail.com>
2016-08-26 18:27:18 +0900

    VideoActivity 툴바 적용중

--- app/src/main/java/com/weddingsingers/wsapp/function/video/video/VideoActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/video/video/VideoActivity.java
@@ -3,19 +3,30 @@ package com.weddingsingers.wsapp.function.video.video;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.weddingsingers.wsapp.R;
 import com.weddingsingers.wsapp.data.SearchResult;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class VideoActivity extends AppCompatActivity {
     public static final String EXTRA_SEARCH_RESULT = "searchResult";
+
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
+       toolbar = (Toolbar)findViewById(R.id.video_toolbar);
 
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 }
