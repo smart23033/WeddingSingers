b2a8425f7f27b2867411535a36df3ff6ad2d70c7
KSJ <smart23033@gmail.com>
2016-08-26 09:37:57 +0900

    view_singer 생성, fragment별로 마진을 fragment_horizontal/vertical_margin으로 이름 변경

--- app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerViewHolder.java
+++ app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerViewHolder.java
@@ -1,7 +1,22 @@
 package com.weddingsingers.wsapp.data.viewholder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by Tacademy on 2016-08-25.
  */
-public class SingerViewHolder {
+public class SingerViewHolder extends RecyclerView.ViewHolder{
+    ImageView  imageView;
+    TextView titleTextView;
+    TextView singerNameTextView;
+    TextView dateTextView;
+    TextView hitTextView;
+    TextView favoriteTextView;
+
+    public SingerViewHolder(View itemView) {
+        super(itemView);
+    }
 }
