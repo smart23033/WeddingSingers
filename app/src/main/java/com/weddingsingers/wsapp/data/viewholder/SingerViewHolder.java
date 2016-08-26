8b343c86991ec44fb58521867e59a95bd6e8024e
KSJ <smart23033@gmail.com>
2016-08-26 12:58:06 +0900

    SearchResultAdapter 수정중

--- app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerViewHolder.java
+++ app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerViewHolder.java
@@ -5,18 +5,45 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.weddingsingers.wsapp.R;
+import com.weddingsingers.wsapp.data.Singer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by Tacademy on 2016-08-25.
  */
 public class SingerViewHolder extends RecyclerView.ViewHolder{
-    ImageView  imageView;
+
+    @BindView(R.id.view_singer_iv_thumbnail)
+    ImageView imageView;
+
+    @BindView(R.id.view_singer_tv_title)
     TextView titleTextView;
+
+    @BindView(R.id.view_singer_tv_singer_name)
     TextView singerNameTextView;
+
+    @BindView(R.id.view_singer_tv_date)
     TextView dateTextView;
+
+    @BindView(R.id.view_singer_tv_hit)
     TextView hitTextView;
+
+    @BindView(R.id.view_singer_tv_num_of_favorite)
     TextView favoriteTextView;
 
     public SingerViewHolder(View itemView) {
         super(itemView);
+        ButterKnife.bind(this,itemView);
     }
+
+//    과연 이게 싱어를 받아오는 것일까?
+    Singer singer;
+
+    public void setSinger(Singer singer){
+        this.singer = singer;
+    }
+
 }
--- app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchResultAdapter.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchResultAdapter.java
@@ -1,8 +1,45 @@
 package com.weddingsingers.wsapp.function.search.search;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.weddingsingers.wsapp.R;
+import com.weddingsingers.wsapp.data.Singer;
+import com.weddingsingers.wsapp.data.viewholder.SingerViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Tacademy on 2016-08-25.
  */
-public class SearchResultAdapter{
-//리싸이클러 만들자!!
+public class SearchResultAdapter extends RecyclerView.Adapter<SingerViewHolder>{
+    List<Singer> items = new ArrayList<>();
+
+    public void add(Singer singer){
+        items.add(singer);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public SingerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_search_result_recycler,parent,false);
+
+        return new SingerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SingerViewHolder holder, int position) {
+        holder.setSinger(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+
 }
