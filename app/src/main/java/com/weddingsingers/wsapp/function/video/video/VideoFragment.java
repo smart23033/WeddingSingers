3536a56ba467f977d6da13f345968aaf76b78fc4
SJSJ <smart23033@gmail.com>
2016-08-27 13:41:00 +0900

    activity_main_app, activity_main_content로 이름 변경

--- app/src/main/java/com/weddingsingers/wsapp/function/video/video/VideoFragment.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/video/video/VideoFragment.java
@@ -3,11 +3,17 @@ package com.weddingsingers.wsapp.function.video.video;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaControllerCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.weddingsingers.wsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,12 +26,24 @@ public class VideoFragment extends Fragment {
 
     }
 
+    @BindView(R.id.video_vv_video)
+    VideoView videoView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false);
+        View view =  inflater.inflate(R.layout.fragment_video, container, false);
+        ButterKnife.bind(this,view);
+
+        videoView.setVideoPath("https://www.youtube.com/watch?v=zQhZUGNR6l4");
+
+        final MediaController mediaController = new MediaController(getContext());
+
+        videoView.setMediaController(mediaController);
+
+        return  view;
+
     }
 
 }
