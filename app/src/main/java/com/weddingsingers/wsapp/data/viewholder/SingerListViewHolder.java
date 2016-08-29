5e1b2a042c22d7351b43991e76500dbca6249353
KSJ <smart23033@gmail.com>
2016-08-29 20:24:33 +0900

    SingerListViewHoler에서 프로필 백그라운드 지웠음. 동그라미로 나옴!

--- app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerListViewHolder.java
+++ app/src/main/java/com/weddingsingers/wsapp/data/viewholder/SingerListViewHolder.java
@@ -3,6 +3,7 @@ package com.weddingsingers.wsapp.data.viewholder;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.makeramen.roundedimageview.RoundedImageView;
@@ -11,6 +12,7 @@ import com.weddingsingers.wsapp.data.SingerList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Tacademy on 2016-08-29.
@@ -32,6 +34,9 @@ public class SingerListViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.view_reserved_singer_tv_detail_songs)
     TextView songsView;
 
+    @BindView(R.id.view_reserved_singer_btn_pay)
+    Button payBtn;
+
     public interface OnPayBtnClickListener {
         public void onPayBtnClick(View view, SingerList singerList, int position);
     }
@@ -51,7 +56,7 @@ public class SingerListViewHolder extends RecyclerView.ViewHolder {
         singerImageView.setOval(true);
         singerImageView.setBackgroundColor(Color.LTGRAY);
 
-        itemView.setOnClickListener(new View.OnClickListener() {
+        payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (listener != null) {
@@ -60,6 +65,7 @@ public class SingerListViewHolder extends RecyclerView.ViewHolder {
             }
         });
     }
+
 
     SingerList singerList;
 
