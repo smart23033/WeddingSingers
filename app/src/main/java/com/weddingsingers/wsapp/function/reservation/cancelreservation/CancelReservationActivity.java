c7bd13a76b8fe3df8b08eb1fc7ee7a9ad62cbfbe
KSJ <smart23033@gmail.com>
2016-08-30 11:28:36 +0900

    CancelReservation 완성

--- app/src/main/java/com/weddingsingers/wsapp/function/reservation/cancelreservation/CancelReservationActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/reservation/cancelreservation/CancelReservationActivity.java
@@ -1,15 +1,38 @@
 package com.weddingsingers.wsapp.function.reservation.cancelreservation;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.weddingsingers.wsapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CancelReservationActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.cancel_reservation_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cancel_reservation);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction();
+        CancelReservationFragment cancelReservationFragment = new CancelReservationFragment();
+        ft.add(R.id.act_cancel_reservation_fl_container,cancelReservationFragment);
+        ft.commit();
     }
 }
--- app/src/main/java/com/weddingsingers/wsapp/function/reservation/cancelreservation/CancelReservationFragment.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/reservation/cancelreservation/CancelReservationFragment.java
@@ -1,109 +1,81 @@
 package com.weddingsingers.wsapp.function.reservation.cancelreservation;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.makeramen.roundedimageview.RoundedImageView;
 import com.weddingsingers.wsapp.R;
+import com.weddingsingers.wsapp.main.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CancelReservationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CancelReservationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
 public class CancelReservationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private static final String ARG_MESSAGE = "param1";
+    private String message;
+    private static CancelReservationFragment instance;
 
     public CancelReservationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CancelReservationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CancelReservationFragment newInstance(String param1, String param2) {
+
+    @BindView(R.id.cancel_reservation_riv_profile)
+    RoundedImageView singerImageView;
+
+
+    public static CancelReservationFragment newInstance(String message) {
         CancelReservationFragment fragment = new CancelReservationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_MESSAGE, message);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cancel_reservation, container, false);
+        View view = inflater.inflate(R.layout.fragment_cancel_reservation, container, false);
+
+        ButterKnife.bind(this, view);
+
+        singerImageView.mutateBackground(true);
+        singerImageView.setOval(true);
+        singerImageView.setBackgroundColor(Color.LTGRAY);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    @OnClick(R.id.cancel_reservation_btn_cancel)
+    void onCancelBtnClicked() {
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Cancel complete")
+                .setMessage("Cancel complete,\ncheck your account please")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        moveMainActivity();
+                    }
+                });
+        dialog = builder.create();
+        dialog.show();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    private void moveMainActivity() {
+        startActivity(new Intent(getActivity(), MainActivity.class));
+        getActivity().finish();
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
