14f6712a2f33a806218fb342fa876ac3619b62e4
KSJ <smart23033@gmail.com>
2016-08-30 12:42:03 +0900

    paymentActivity 채워넣음

--- app/src/main/java/com/weddingsingers/wsapp/function/payment/payment/PaymentActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/payment/payment/PaymentActivity.java
@@ -1,15 +1,38 @@
 package com.weddingsingers.wsapp.function.payment.payment;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.weddingsingers.wsapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class PaymentActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.payment_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        FragmentTransaction ft = getSupportFragmentManager()
+                .beginTransaction();
+        PaymentFragment paymentFragment = new PaymentFragment();
+        ft.add(R.id.act_cancel_reservation_fl_container,paymentFragment);
+        ft.commit();
     }
 }
