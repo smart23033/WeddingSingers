35f63c56f637bd8669b73928432c5d34a3aefe94
KSJ <smart23033@gmail.com>
2016-08-31 16:07:09 +0900

    DayOffFragment 달력만들기..했음

--- app/src/main/java/com/weddingsingers/wsapp/function/schedulemgm/schedulemgm/DayOffFragment.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/schedulemgm/schedulemgm/DayOffFragment.java
@@ -2,23 +2,26 @@ package com.weddingsingers.wsapp.function.schedulemgm.schedulemgm;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import com.weddingsingers.wsapp.R;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,7 +30,8 @@ import butterknife.OnClick;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DayOffFragment extends Fragment {
+public class DayOffFragment extends Fragment implements
+        OnDateSelectedListener {
 
     private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
 
@@ -42,8 +46,8 @@ public class DayOffFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-    }
 
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,24 +57,36 @@ public class DayOffFragment extends Fragment {
 
         ButterKnife.bind(this,view);
 
+//        미완
+        calendarView.getSelectedDates();
+
+        calendarView.setOnDateChangedListener(this);
         calendarView.setSelectionMode(MaterialCalendarView.SELECTION_MODE_MULTIPLE);
 
         return view;
     }
 
-    private String getSelectedDatesString() {
-        CalendarDay date = calendarView.getSelectedDate();
-        if (date == null) {
-            return "No Selection";
+    ArrayList<CalendarDay> dates = new ArrayList<>();
+
+    @OnClick(R.id.day_off_btn_set)
+    void onSetBtnClicked(){
+        for(CalendarDay d : dates) {
+            Log.i("DayOffFragment", "dates : " + FORMATTER.format(d.getDate()));
+            calendarView.setDateSelected(d,true);
         }
-        return FORMATTER.format(date.getDate());
+
     }
 
-    @OnClick(R.id.day_off_btn_ok)
-    void onOkBtnClicked(){
-        Toast.makeText(getContext(),getSelectedDatesString(),Toast.LENGTH_SHORT).show();
-    }
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        Toast.makeText(getContext(),"date : " + date,Toast.LENGTH_SHORT).show();
+       if(dates.contains(date)){
+          dates.remove(date);
+       }else{
+           dates.add(date);
+       }
 
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
