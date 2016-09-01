9fd8dd4018e17368075fea51f2bb19b1a3a258fc
KSJ <smart23033@gmail.com>
2016-09-01 18:07:08 +0900

    push test 2

--- app/src/main/java/com/weddingsingers/wsapp/login/LoginIntroFragment.java
+++ app/src/main/java/com/weddingsingers/wsapp/login/LoginIntroFragment.java
@@ -6,14 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.weddingsingers.wsapp.R;
+
+import butterknife.BindView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginIntroFragment extends Fragment {
 
+    @BindView(R.id.login_intro_btn_login)
+    Button loginBtn;
 
     public LoginIntroFragment() {
         // Required empty public constructor
