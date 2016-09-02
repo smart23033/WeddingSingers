ada0d2c1c609a08fac4924af6065947dd7fceb30
DK046 <verano@korea.com>
2016-09-02 15:00:07 +0900

    채팅

---
+++ app/src/main/java/com/weddingsingers/wsapp/data/ChatContract.java
@@ -0,0 +1,28 @@
+package com.weddingsingers.wsapp.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Tacademy on 2016-09-02.
+ */
+public class ChatContract {
+    public interface ChatUser extends BaseColumns {
+        public static final String TABLE = "chatuser";
+        public static final String COLUMN_SERVER_ID = "sid";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_LAST_DATE = "lastDate";
+        public static final String COLUMN_LAST_MESSAGE_ID = "lastid";
+    }
+
+    public interface ChatMessage extends BaseColumns {
+        public static final int TYPE_SEND = 0;
+        public static final int TYPE_RECEIVE = 1;
+        public static final int TYPE_DATE = 2;
+
+        public static final String TABLE = "chatmessage";
+        public static final String COLUMN_USER_ID = "uid";
+        public static final String COLUMN_TYPE = "type";
+        public static final String COLUMN_MESSAGE = "message";
+        public static final String COLUMN_CREATED = "created";
+    }
+}
--- app/src/main/java/com/weddingsingers/wsapp/main/chatting/ChattingListFragment.java
+++ app/src/main/java/com/weddingsingers/wsapp/main/chatting/ChattingListFragment.java
@@ -1,29 +1,72 @@
 package com.weddingsingers.wsapp.main.chatting;
 
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.weddingsingers.wsapp.R;
+import com.weddingsingers.wsapp.data.ChatContract;
+import com.weddingsingers.wsapp.function.chatting.chatting.ChattingActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChattingListFragment extends Fragment {
 
+    @BindView(R.id.chatting_list_lv_list)
+    ListView listView;
+
+    SimpleCursorAdapter mAdapter;
 
     public ChattingListFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatting_list, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] from = {ChatContract.ChatUser.COLUMN_NAME, ChatContract.ChatUser.COLUMN_LAST_DATE, ChatContract.ChatMessage.COLUMN_MESSAGE};
+        int[] to = {R.id.view_chat_user_tv_name, R.id.view_chat_user_tv_last_date, R.id.view_chat_user_tv_last_message};
+
+        mAdapter = new SimpleCursorAdapter(getContext(), R.layout.view_chat_user, null, from, to, 0);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_chatting_list, container, false);
+
+        ButterKnife.bind(this, view);
+
+        listView.setAdapter(mAdapter);
+
+        initData();
+
+        return view;
+    }
+
+    public void initData() {
+
+    }
+
+    @OnItemClick(R.id.chatting_list_lv_list)
+    public void onItemClick(int position, long id) {
+        startActivity(new Intent(getActivity(), ChattingActivity.class));
+    }
 }
