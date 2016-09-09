e553f1b425af9ae13cdd5f32f69b6dcbf329e62e
KSJ <smart23033@gmail.com>
2016-09-09 14:08:40 +0900

    video 요청 시작

---
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/EmptyResultFragment.java
@@ -0,0 +1,30 @@
+package com.weddingsingers.wsapp.function.search.search;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.weddingsingers.wsapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmptyResultFragment extends Fragment {
+
+
+    public EmptyResultFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_empty_result, container, false);
+    }
+
+}
--- app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/function/search/search/SearchActivity.java
@@ -35,6 +35,8 @@ public class SearchActivity extends AppCompatActivity {
     final static String FRAG_RECENT_SEARCH = "RecentSearchFragment";
     final static String FRAG_FILTER = "FilterFragment";
     final static String FRAG_SEACH_RESULT = "SearchResultFragment";
+    final static String FRAG_EMPTY_RESULT = "EmptyResultFragment";
+    final static int INITIAL_VALUE = 0;
 
     final static String KEY_SEARCH = "Search";
 
@@ -87,7 +89,7 @@ public class SearchActivity extends AppCompatActivity {
 
                 Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.act_search_fl_container);
 
-                if (currentFragment.getTag() != FRAG_SEACH_RESULT) {
+                if (currentFragment.getTag() == FRAG_FILTER || currentFragment.getTag() == FRAG_RECENT_SEARCH) {
 //               searchResultFragment가 아닌 프레그먼트(filterFrag랑 RecentSearchFrag)에서 search버튼 누를 때
                     keywordView.setText(keywordInput.getText().toString());
                     keywordView.setVisibility(View.VISIBLE);
@@ -104,19 +106,50 @@ public class SearchActivity extends AppCompatActivity {
                         search.setStartDate(((FilterFragment) currentFragment).search.getStartDate());
                         search.setEndDate(((FilterFragment) currentFragment).search.getEndDate());
                         search.setTheme(((FilterFragment) currentFragment).search.getTheme());
+
+                    } else if (currentFragment.getTag() == FRAG_RECENT_SEARCH) {
+                        search.setLocation(INITIAL_VALUE);
+                        search.setComposition(INITIAL_VALUE);
+                        search.setPrice(INITIAL_VALUE);
+//                        search.setStartDate(((FilterFragment) currentFragment).search.getStartDate());
+//                        search.setEndDate(((FilterFragment) currentFragment).search.getEndDate());
+                        search.setTheme(INITIAL_VALUE);
+
                     }
 
-                    Log.i("SearchActivity","keyword : " + search.getKeyword());
-                    Log.i("SearchActivity","location : " + search.getLocation());
-                    Log.i("SearchActivity","price : " + search.getPrice());
-                    Log.i("SearchActivity","composition : " + search.getComposition());
-                    Log.i("SearchActivity","theme : " + search.getTheme());
+                    Log.i("SearchActivity", "keyword : " + search.getKeyword());
+                    Log.i("SearchActivity", "location : " + search.getLocation());
+                    Log.i("SearchActivity", "price : " + search.getPrice());
+                    Log.i("SearchActivity", "composition : " + search.getComposition());
+                    Log.i("SearchActivity", "theme : " + search.getTheme());
 
-                    FragmentTransaction ft = getSupportFragmentManager()
-                            .beginTransaction();
-                    SearchResultFragment searchResultFragment = SearchResultFragment.newInstance(search);
-                    ft.replace(R.id.act_search_fl_container, searchResultFragment, FRAG_SEACH_RESULT);
-                    ft.commit();
+                    SearchRequest request = new SearchRequest(this, search);
+                    NetworkManager.getInstance().getNetworkData(request, new NetworkManager.OnResultListener<NetworkResult<List<SearchResult>>>() {
+                        @Override
+                        public void onSuccess(NetworkRequest<NetworkResult<List<SearchResult>>> request, NetworkResult<List<SearchResult>> result) {
+                            Toast.makeText(SearchActivity.this, "Search Success!", Toast.LENGTH_SHORT).show();
+
+                            if (result.getResult().size() != 0) {
+                                FragmentTransaction ft = getSupportFragmentManager()
+                                        .beginTransaction();
+                                SearchResultFragment searchResultFragment = SearchResultFragment.newInstance(search);
+                                ft.replace(R.id.act_search_fl_container, searchResultFragment, FRAG_SEACH_RESULT);
+                                ft.commit();
+                            } else {
+                                Log.i("SearchResultFragment", "result.getResult() 없다.");
+                                FragmentTransaction ft = getSupportFragmentManager()
+                                        .beginTransaction();
+                                EmptyResultFragment emptyResultFragment = new EmptyResultFragment();
+                                ft.replace(R.id.act_search_fl_container, emptyResultFragment,FRAG_EMPTY_RESULT);
+                                ft.commit();
+                            }
+                        }
+
+                        @Override
+                        public void onFail(NetworkRequest<NetworkResult<List<SearchResult>>> request, int errorCode, String errorMessage, Throwable e) {
+                            Toast.makeText(SearchActivity.this, "Search fail : " + errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
 
                 } else {
