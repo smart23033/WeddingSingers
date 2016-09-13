a6c205264844be3637cb5e6849d85307244cab8e
KSJ <smart23033@gmail.com>
2016-09-13 14:22:17 +0900

    Facebook 연동중

--- app/src/main/java/com/weddingsingers/wsapp/MyApplication.java
+++ app/src/main/java/com/weddingsingers/wsapp/MyApplication.java
@@ -3,6 +3,9 @@ package com.weddingsingers.wsapp;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 /**
  * Created by Tacademy on 2016-08-25.
  */
@@ -14,6 +17,8 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 
     public static Context getContext() {
