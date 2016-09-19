91774dbfb9addb7fbdb037881b47d10e88614ce5
KSJ <smart23033@gmail.com>
2016-09-19 17:26:46 +0900

    자동로그인 구현중 세션만료되었으나 sharedPref에 존재한다면? 실험중

--- app/src/main/java/com/weddingsingers/wsapp/SplashActivity.java
+++ app/src/main/java/com/weddingsingers/wsapp/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.weddingsingers.wsapp.data.NetworkResult;
@@ -19,6 +20,11 @@ import com.weddingsingers.wsapp.request.ProfileRequest;
 
 public class SplashActivity extends AppCompatActivity {
 
+    int id;
+    int type;
+    String email;
+    String name;
+    String photoURL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +34,27 @@ public class SplashActivity extends AppCompatActivity {
         NetworkManager.getInstance().getNetworkData(profileRequest, new NetworkManager.OnResultListener<NetworkResult<User>>() {
             @Override
             public void onSuccess(NetworkRequest<NetworkResult<User>> request, NetworkResult<User> result) {
-                moveMainActivity();
+                Log.i("SplashActivity","profileRequest success");
+
+                id = result.getResult().getId();
+                type = result.getResult().getType();
+                email = result.getResult().getEmail();
+                name = result.getResult().getName();
+                photoURL = result.getResult().getPhotoURL();
+
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                intent.putExtra(MainActivity.EXTRA_USER_ID, id);
+                intent.putExtra(MainActivity.EXTRA_USER_TYPE, type);
+                intent.putExtra(MainActivity.EXTRA_USER_NAME, name);
+                intent.putExtra(MainActivity.EXTRA_USER_EMAIL, email);
+                intent.putExtra(MainActivity.FRAG_NAME, MainActivity.FRAG_MAIN);
+                moveMainActivity(intent);
+
             }
 
             @Override
             public void onFail(NetworkRequest<NetworkResult<User>> request, int errorCode, String errorMessage, Throwable e) {
+                Log.i("SplashActivity","profileRequest fail");
                 loginSharedPreference();
             }
         });
@@ -40,11 +62,21 @@ public class SplashActivity extends AppCompatActivity {
     }
 
 
+    private void moveMainActivity(final Intent intent) {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 500);
+    }
+
     private void moveMainActivity() {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
         }, 500);
@@ -53,6 +85,7 @@ public class SplashActivity extends AppCompatActivity {
     Handler mHandler = new Handler(Looper.getMainLooper());
 
     private void loginSharedPreference() {
+        Log.i("SplashActivity","isAutoLogin : " + isAutoLogin());
         if (isAutoLogin()) {
             processAutoLogin();
         } else {
@@ -61,6 +94,7 @@ public class SplashActivity extends AppCompatActivity {
     }
     private boolean isAutoLogin() {
         String email = PropertyManager.getInstance().getEmail();
+        Log.i("SplashActivity","email : " + email);
         return !TextUtils.isEmpty(email);
     }
 
